3fa5ccb9f67cdd4210ed9449af893ec55dbdae57
sn-datawizard <stefan.stohl@gmx.de>
2024-03-11 00:15:24 +0100

    Add dynamic webpage node.js

---
+++ restaurantwebpage/src/index.js
@@ -0,0 +1,96 @@
+
+function main() {
+    createContent();
+}
+
+function createContent() {
+    createHeader();
+    createWelcomeText();
+    createOpeningHours();
+    createLocationText()
+}
+
+function createHeader() {
+    var idContainer = 'header-container';
+    var element = 'h1';
+    var elementClass = 'header';
+    var text = 'XYZ Korean Chicken'
+
+    createContentBlock(idContainer, element, elementClass, text);
+}
+
+function createWelcomeText() {
+    var idContainer = 'welcome-container';
+    var element = 'p';
+    var elementClass = 'welcome-text';
+    var text = 
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a purus a est faucibus sollicitudin. Vivamus non tellus eget arcu. ipsum. ' +
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a purus a est faucibus sollicitudin. Vivamus non tellus eget arcu. ipsum. ' +
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a purus a est faucibus sollicitudin. Vivamus non tellus eget arcu. ipsum. ' +
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a purus a est faucibus sollicitudin. Vivamus non tellus eget arcu. ipsum.'
+
+    createContentBlock(idContainer, element, elementClass, text);
+}
+
+function createOpeningHours() {
+    var idContainer = 'opening-container';
+    var element = 'p';
+    var elementClass = 'opening-text';
+    
+    createContentBlock(idContainer, element, elementClass, null);
+
+
+    const openingContainer = document.getElementById('opening-container');
+    openingContainer.replaceChildren();
+    
+    
+    var schedule = [
+        'Monday: 2pm - 10pm',
+        'Tuesday: 2pm - 10pm',
+        'Wednesday: 2pm - 10pm',
+        'Thursday: 2pm - 10pm',
+        'Friday: 2pm - 10pm',
+        'Saturday: 11am - 7pm',
+        'Sunday: 11am - 7pm'
+    ];
+
+    for (var i = 0; i < schedule.length; i++) {
+        hourDiv = openingContainer.appendChild(document.createElement('div'));
+        hourDiv.setAttribute('class', 'opening-hours');
+        hourDiv.textContent = schedule[i];
+    }
+
+}
+
+function createLocationText() {
+    var idContainer = 'location-container';
+    var element = 'p';
+    var elementClass = 'location-text';
+    var text = '123 Forest Drive, Forestville, TX'
+
+    createContentBlock(idContainer, element, elementClass, text);
+}
+
+
+
+///////////////////////
+// Helper Functions //
+/////////////////////
+
+function createContentBlock(id, element, className, textContent) {
+    const content = document.getElementById('content');
+    const container = content.appendChild(document.createElement('div'));
+    container.setAttribute('id', id);
+    container.setAttribute('class', 'container')
+
+    const header = container.appendChild(document.createElement(element));
+    header.setAttribute('class', className);
+    header.textContent = textContent;
+}
+
+
+
+
+
+
+main();
