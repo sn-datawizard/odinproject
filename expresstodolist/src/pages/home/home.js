7bb01ae6e4337c858b08caaf4337be21039a0836
sn-datawizard <stefan.stohl@gmx.de>
2024-03-19 22:25:56 +0100

    refactor home.js

--- expresstodolist/src/pages/home/home.js
+++ expresstodolist/src/pages/home/home.js
@@ -8,9 +8,6 @@ function addEventListenerButton() {
     var loginButton = document.getElementById('login');
     var signupButton = document.getElementById('signup');
 
-    // loginButton.addEventListener('click', function() {redirectLogin()});
-    // signupButton.addEventListener('click', function() {redirectSignup()});
-
     loginButton.addEventListener('click', function() {helper.redirectPage('/login')});
     signupButton.addEventListener('click', function() {helper.redirectPage('/signup')});
 
