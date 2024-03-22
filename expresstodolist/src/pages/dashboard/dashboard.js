a3c632ede29f9f7846f6cd719ebc2dfe06c859b8
sn-datawizard <stefan.stohl@gmx.de>
2024-03-23 00:27:01 +0100

    Implement function to add task and delete task

--- expresstodolist/src/pages/dashboard/dashboard.js
+++ expresstodolist/src/pages/dashboard/dashboard.js
@@ -9,10 +9,25 @@ function addRow() {
     var content = document.getElementsByClassName('content')[0];
     var row = content.appendChild(document.createElement('div'));
     row.setAttribute('class', 'row');
+
+    addTask(row);
 }
 
+function addTask(row) {
+    var task = row.appendChild(document.createElement('div'));
+    task.setAttribute('class', Math.random() * 10);
 
+    var button = task.appendChild(document.createElement('button'));
+    button.textContent = 'DELETE';
 
+    button.addEventListener('click', function() {deleteTask(button)})
+
+}
+
+function deleteTask(button) {
+    var parent = button.parentElement
+    parent.remove();
+}
 
 
 main();
