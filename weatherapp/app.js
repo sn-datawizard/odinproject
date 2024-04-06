6543abd8783a6b2972ee54eb5de91d8ddd1dbf36
sn-datawizard <stefan.stohl@gmx.de>
2024-04-06 14:27:37 +0200

    Initialize weather app project

---
+++ weatherapp/app.js
@@ -0,0 +1,23 @@
+
+const express = require('express'); 
+const path = require('path');
+const app = express(); 
+const port = 3000; 
+
+app.use('/src', express.static(path.join(__dirname, 'src')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/pages/home/home.html'));
+  });
+
+
+
+
+app.listen(port, (error) =>{ 
+    if(!error) 
+    console.log(`Server is listening on port ${port} and can be accessed at http://localhost:3000/ `);
+    else 
+        console.log("Error occurred, server can't start", error); 
+    } 
+);
