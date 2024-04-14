a6787bf444f5908851bd99690b89bf03343e2dc7
sn-datawizard <stefan.stohl@gmx.de>
2024-04-14 14:41:34 +0200

    Refactor to make API call on client side to dynamically display data

--- weatherapp/src/pages/home/home.js
+++ weatherapp/src/pages/home/home.js
@@ -1,24 +1,31 @@
-
+import { apiKey } from '../../utils/apikey.js'
 
 async function main() {
-    const fetch = await fetchData();
-
-    displayLocation(fetch);
-    displayTempC(fetch);
-    displayTempF(fetch);
-    displayCondition(fetch);
+    const api = await apiKey;
+    const submitButton = document.getElementById('button-submit');
+    submitButton.addEventListener('click', async function() {displayMeasures(api);});
 }
 
-async function fetchData() {
-    try {
-        const response = await fetch('/data');
-        const data = await response.json();
+async function displayMeasures(api) {
+        const fetch = await fetchAPI(api);
 
-        return data;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-};
+        displayLocation(fetch);
+        displayTempC(fetch);
+        displayTempF(fetch);
+        displayCondition(fetch);
+}
+
+async function fetchAPI(key) {
+    const city = await getInput();
+    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=' + key + '&q=' + city, {mode: 'cors'})
+    const data = await response.json();
+    return data;
+}
+
+async function getInput() {
+    const formInput = document.getElementById('input-city');
+    return formInput.value;
+}
 
 function displayLocation(i) {
     const contentCity = document.getElementsByClassName('content-city')[0];
@@ -51,12 +58,12 @@ function changeIcon(text) {
         'Mist': '/public/images/indicator-mist.png',
         'Rainy': '/public/images/indicator-other.png',
         'Clear': '/public/images/indicator-other.png',
-        'Overcast': '/public/images/indicator-other.png'
+        'Overcast': '/public/images/indicator-other.png',
+        'Other': '/public/images/indicator-other.png'
     };
 
     if (conditionText in iconMap) {
         const iconClass = iconMap[conditionText];
-        console.log(iconClass);
         
         const imageContainer = document.getElementsByClassName('header-indicator')[0];
         const image = iconMap[conditionText];
@@ -64,11 +71,14 @@ function changeIcon(text) {
         imgElement.src = image; 
         imageContainer.appendChild(imgElement);
         
-    } else {
-        console.log('Weather condition not in map');
+    } else {   
+        const imageContainer = document.getElementsByClassName('header-indicator')[0];
+        const image = iconMap['Other'];
+        const imgElement = document.createElement('img');
+        imgElement.src = image; 
+        imageContainer.appendChild(imgElement);
     }
 }
-
 
 
 main();
